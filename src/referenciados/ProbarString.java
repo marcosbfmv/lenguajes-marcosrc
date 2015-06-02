d57a454553bb2c8df6a4d2e16f3156a6924029f5
T-107 <T-107@PC250>
2015-06-01 19:43:36 -0500

    Clase 01 de junio de 2015

---
+++ src/referenciados/ProbarString.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package referenciados;
+
+/**
+ *
+ * @author T-107
+ */
+public class ProbarString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String hola="hola";
+       String hola2=new String("Hola 2");
+        int []entero= {4,24,12,-7};
+        int otro[]={34,3};
+        String bb[]={"primero","este otro","uno mas","el ultimo"};
+        System.out.println(bb.length);
+        
+        int mas[]=new int[3];
+        int primitivo;
+        System.out.println(mas[1]);
+    }
+    
+}
