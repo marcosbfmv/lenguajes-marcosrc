b5a7d89b37cd63cde87ea3933f3493dbd5b4b8b7
T-107 <T-107@PC250>
2015-06-08 19:48:16 -0500

    Clase del 08 de junio del 2015

---
+++ src/herencia/Animal.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author T-107
+ */
+public abstract class Animal  {
+    
+     private String nombre;
+     private int eda;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEda() {
+        return eda;
+    }
+
+    public void setEda(int eda) {
+        this.eda = eda;
+    }
+    
+    
+    
+    
+}
---
+++ src/herencia/Chapultepec.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author T-107
+ */
+public class Chapultepec {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // Animal raro=new Animal();
+       //        raro.setEda(2);
+       //        raro.setNombre("Rareza");
+        Tortuga animal1=new Tortuga();
+                animal1.setNombre("Japonesa");
+                animal1.setEda(12);
+        Leon animal2=new Leon();
+                animal2.setNombre("Afrcano");
+                animal2.setEda(20);
+                
+                
+        
+                
+    }
+    
+}
