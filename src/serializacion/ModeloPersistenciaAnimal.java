c38014fef240f0b2d7786bc65e3b7f241bb47d64
T-107 <T-107@PC250>
2015-07-23 19:41:13 -0500

    Clase del 23 de julio del 2015

--- src/serializacion/ModeloPersistenciaAnimal.java
+++ src/serializacion/ModeloPersistenciaAnimal.java
@@ -10,17 +10,41 @@ package serializacion;
  * @author T-107
  */
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModeloPersistenciaAnimal {
-
+    
+    String ruta="D:\\Animal\\animalitos.xxx";
+    ArrayList<Animal> animales;
+    public ModeloPersistenciaAnimal(){
+        animales=new ArrayList<Animal>();
+    }
     public void guardar(Animal animal) throws Exception{
         // paso 1 crear el archivo donde se guardara el animal
         
-        File file =new File("D:\\Animal\\animalitos.xxx");
+        File file =new File(ruta);
+        if(file.exists()){
+            
+        animales=buscarTodos();
+            
+            
+        }
          FileOutputStream fos=new FileOutputStream(file);
          ObjectOutputStream oos=new ObjectOutputStream(fos);
-         oos.writeObject(animal);
+         animales.add(animal);
+         oos.writeObject(animales);
          System.out.println("Animalito comprimido con exito");
         
+    }
+    
+    public ArrayList<Animal> buscarTodos()throws  Exception{
+        
+        Animal a=new Animal();
+        File f=new File(ruta);
+        FileInputStream fis=new FileInputStream(f);
+        ObjectInputStream ois=new ObjectInputStream(fis);
+        animales= (ArrayList<Animal>) ois.readObject();
+        return animales;
     }
 }
---
+++ src/serializacion/Zoologico.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializacion;
+
+/**
+ *
+ * @author T-107
+ */
+public class Zoologico {
+    public static void main(String[] args)throws Exception{
+        ModeloPersistenciaAnimal m=new ModeloPersistenciaAnimal();
+       Animal a=new Animal();
+              a.setNombre("Tigre");
+              a.setCarnivoro(true);
+              m.guardar(a);
+              
+        for (Animal ani:m.buscarTodos()){
+            System.out.println(ani.getNombre());
+        
+    }
+        
+    }
+    
+}
